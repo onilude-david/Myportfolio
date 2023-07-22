03873fea465f11c0efb26a19696c936125768e12
Kazeem Quadri <quadrikazeem01@gmail.com>
2023-07-22 09:43:51 +0100

    deployment fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  //Comment this line when you want to deploy to a different directory
+  base: '/dev',
+});
